d33f7aa26cd2e41dc5e34626f7b131d7a0abcc99
Kannan D <kannan.dakm@gmail.com>
2017-08-03 12:17:37 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,9 +2,17 @@ console.log('Loaded!');
 //change the text of main-text div
 var element=document.getElementById('main-text');
 element.innerHTML="New value";
+
+var marginLeft=0;
+
+function moveRight(){
+    marginLeft=marginLeft+10;
+    img.style.marginLeft=marginLeft+'px';
+}
+
 //move the image
 var img=document.getElementById('madi');
 img.onclick=function(){
-    img.style.marginLeft='100px';
-   // var interval=setInterval(moveRight,100);
+   // img.style.marginLeft='100px';
+    var interval=setInterval(moveRight,100);
 };
