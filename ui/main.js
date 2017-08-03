75d865c8bd076db9df40579242dd8dc6bf166471
Kannan D <kannan.dakm@gmail.com>
2017-08-03 13:14:39 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -16,3 +16,13 @@ img.onclick=function(){
    // img.style.marginLeft='100px';
     var interval=setInterval(moveRight,50);
 };
+
+var button=document.getElementById('counter');
+var counter=0;
+
+button.onclick=function(){
+    counter=counter+1;
+    var span=document.getElementById('count');
+    span.innerHTML=counter.toString();
+
+};
