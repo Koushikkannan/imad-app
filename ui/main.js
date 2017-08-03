858e76bbb72b8ae219c028ed4c73455b1529938b
Kannan D <kannan.dakm@gmail.com>
2017-08-03 23:34:35 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -52,10 +52,10 @@ submit.onclick=function()
     //make a request to the server and send the name
     //capture a list of names and render it as a list
     var names=['name1','name2','name3','name4'];
-    var list=" ";
+    var list='';
     for(var i=0;i<names.length;i++)
     {
-        list='<li>'+names[i]+'</li>';
+        list=='<li>'+names[i]+'</li>';
     }
 var ul=document.getElementById('namelist');
 ul.innerHTML=list;
